b5f6a4a0f2946569c4f1826086eaf5c831a536f2
Anson-Mah-ACITECH <ansonmah212@gmail.com>
2025-11-09 00:48:44 -0500

    Used w3-include for navbar

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 // Boolean that determines whether the mini navigation menu will be shown or not.
 let nav_shown = false;
 
-// Event Listener fires when hamburger button is clicked.
-document.getElementById('hamburger_button').addEventListener('click', ()=> {
+function toggle_hamburger_button() {
 	nav_shown = !nav_shown; // Toggles the boolean
 	if (nav_shown==true) {
 		document.getElementById('nav_small').style.display = 'grid';
 	} else {
 		document.getElementById('nav_small').style.display = 'none';
 	}
-})
+}
+
+// Directly ripped from W3-Schools
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+includeHTML()
